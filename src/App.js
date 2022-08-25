35b99e7106c2af5be131708c7bc1b4f622152f18
Your Name <you@example.com>
2022-08-25 04:22:43 -1200

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import { motion, AnimatePresence} from "framer-motion";
+import {useRef} from "react";
+import images from "./image";
+
+function App() {
+  
+  const carousel = useRef();
+  
+  // useEffect(()=>{
+  //   setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+  // }, []);
+  return (
+    <div className="App">
+      
+      
+      <motion.h1 animate={{ x: [0, 150, 50], opacity: 1, scale: 1 }} transition={{ duration: 5, delay: 0.3, ease: [0.5, 0.71, 1, 1.5]
+      }}  whileHover={{ scale: 1.2 }}   initial={{ opacity: 0, scale: 0.5 }}     
+      >Animation made easy with Framer Motion</motion.h1>
+
+      <motion.div ref={carousel}  className="carousel" >
+        <motion.div animate={{scale: [1, 1, 1, 2, 1],
+      rotate: [10, 10, 100, 100, 360],
+    }} drag="x" 
+        dragConstraints={{right: 0, left:-900}} 
+        dragTransition={{ bounceStiffness: 600, bounceDamping: 8 }} 
+        className="inner-carousel">
+          {images.map(image =>{
+            return( 
+              <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 2 }}  className='item' 
+              transition={{ duration: 3}}  key={image}>
+            <img src={image} alt=""/>
+          </motion.div> 
+          
+            );
+          })}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/image.js
@@ -0,0 +1,8 @@
+import ethLogo from "./images/ethLogo.png";
+import Solana from "./images/imagesSolana.jpg";
+import poly from "./images/polyLogo.png";
+import btc from "./images/bitcoin_PNG1.png";
+
+
+
+export default[ethLogo, Solana, poly,btc]
