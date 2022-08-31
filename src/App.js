0332c0300a1bd6e777bb4aefcda6ce480eded68d
Your Name <you@example.com>
2022-08-30 13:40:32 -1200

    changes

--- src/App.js
+++ src/App.js
@@ -11,15 +11,15 @@ function App() {
     <div className="App">
       
       
-      <motion.h1 animate={{ x: [50, 150, 50], opacity: 1, scale: 1 }} transition={{ duration: 5, delay: 0.3, ease: [0.5, 0.71, 1, 1.5]
+      {/* <motion.h1 animate={{ x: [50, 150, 50], opacity: 1, scale: 1 }} transition={{ duration: 5, delay: 0.3, ease: [0.5, 0.71, 1, 1.5]
       }}  whileHover={{ scale: 1.2 }}   initial={{ opacity: 0, scale: 0.5 }}     
-      >Animation made easy with Framer Motion</motion.h1>
+      >Animation made easy with Framer Motion</motion.h1> */}
 
       <motion.div   className="carousel" >
         <motion.div animate={{scale: [1, 1,1, 1, 1],
       rotate: [0, 30, 60, 240, 360], 
     }} drag="x" 
-        dragConstraints={{right: 0, left:-1250}} 
+        dragConstraints={{right: 0, left:-1100}} 
         dragTransition={{ bounceStiffness: 600, bounceDamping: 8 }} 
         className="inner-carousel">
           {images.map(image =>{
--- src/image.js
+++ src/image.js
@@ -1,7 +1,7 @@
-import fox from "./images/pexels-fox-1071249.jpg";
-import jill from "./images/jill.jpg";
-import cha1 from "./images/cha1.jpg";
-import cha2 from "./images/cha2.jpg";
+import fox from "./images/pex4.jpg";
+import jill from "./images/khoa.jpg";
+import cha1 from "./images/jill.jpg";
+import cha2 from "./images/juan.jpg";
 
 
 export default[fox, jill, cha1,cha2]
